02f361320084631e3fdde4f6a1046cc822788860
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-04-12 19:58:31 -0500

    Migrating android app to it's own repository.

--- udjserver/udj/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from udj.tests.library import *
-from udj.tests.event import *
-from udj.tests.activeplaylist import *
-from udj.tests.authtests import *
--- udjserver/udj/utils.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def getJSONResponse(jsonContent, status=200):
-  return HttpResponse(jsonContent, status=status, content_type='text/json')
